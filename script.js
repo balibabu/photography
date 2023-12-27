28bf1d78d22db6ed99d9022ba96376e4a633c39f
balibabu <balibabuchauhan1@gmail.com>
2023-12-27 22:43:31 +0545

    sdfsdf

--- script.js
+++ script.js
@@ -3,40 +3,21 @@ document.addEventListener("DOMContentLoaded", function () {
     // Get the reference to the div where images will be displayed
     var imageGallery = document.getElementById("imageGallery");
 
-    // Path to the images folder
-    var imagesFolderPath = 'images/';
+    // Fetch the list of images from the "images" folder
+    fetch("images/")
+        .then(response => response.text())
+        .then(data => {
+            // Parse the HTML content to extract image filenames
+            var parser = new DOMParser();
+            var htmlDoc = parser.parseFromString(data, "text/html");
+            var imageElements = htmlDoc.querySelectorAll("a[href$='.jpg'], a[href$='.png'], a[href$='.jpeg'], a[href$='.gif']");
 
-    // Fetch all images in the "images" folder
-    fetchImages(imagesFolderPath).then(function (imagePaths) {
-        // Loop through the image paths and create image elements
-        imagePaths.forEach(function (path) {
-            // Create an image element
-            var imgElement = document.createElement("img");
-
-            // Set the source attribute to the image path
-            imgElement.src = path;
-
-            // Append the image element to the image gallery
-            imageGallery.appendChild(imgElement);
-        });
-    });
+            // Create image elements for each image in the folder
+            imageElements.forEach(function (imageElement) {
+                var imgElement = document.createElement("img");
+                imgElement.src = "images/" + imageElement.getAttribute("href");
+                imageGallery.appendChild(imgElement);
+            });
+        })
+        .catch(error => console.error("Error fetching images:", error));
 });
-
-// Function to fetch all images in a folder
-async function fetchImages(folderPath) {
-    // Fetch the list of files in the folder
-    var response = await fetch(folderPath);
-    var files = await response.json();
-
-    // Filter only image files (you may need to adjust this filter based on your file types)
-    var imageFiles = files.filter(function (file) {
-        return /\.(jpe?g|png|gif|svg)$/i.test(file.name);
-    });
-
-    // Map file objects to their paths
-    var imagePaths = imageFiles.map(function (file) {
-        return folderPath + file.name;
-    });
-
-    return imagePaths;
-}
